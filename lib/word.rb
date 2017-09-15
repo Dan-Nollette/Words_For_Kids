564ca31ee23ebb0f4fadfd5c2267e9c1159ba723
Dan Nollette <nollette.dan@gmail.com>
2017-09-15 09:02:41 -0700

    Add method names for word class and the describe and do statementa for word spec tests (no blocks yet)

---
+++ lib/word.rb
@@ -0,0 +1,20 @@
+class Word
+  @@all_words = []
+
+  def initialize()
+  end
+
+  def save
+  end
+
+  def self.find
+  end
+
+  def self.clear
+  end
+
+
+  def self.all
+  end
+
+end
---
+++ spec/word_spec.rb
@@ -0,0 +1,38 @@
+require 'rspec'
+require 'word'
+require 'pry'
+
+describe("Contact") do
+   describe("#word_name") do
+     it("checks that word name reader method returns word name") do
+     end
+   end
+   describe("#definitions") do
+     it("checks that definitions reader method returns definitions") do
+     end
+   end
+   describe("#id") do
+     it("checks that id reader method returns id") do
+     end
+   end
+   describe("#definitions=") do
+     it("checks that definitions writer method updates definitions") do
+     end
+   end
+   describe("#save") do
+     it("checks that save method adds word to class variable all_words") do
+     end
+   end
+   describe(".find") do
+     it("checks that find method returns object with given id") do
+     end
+   end
+   describe(".clear") do
+     it("checks that clear method clears the all_words class variable") do
+     end
+   end
+   describe(".all") do
+     it("checks that this method returns the list of all words currently saved") do
+     end
+   end
+end
